093e2ff7dc517af1dab6e7d9f695059fb5ab5769
Levi Sasse <dvnbxd_78@mailbox.tu-berlin.de>
2021-11-24 13:38:36 +0100

    deleted own error completely

--- service/ExceptionHandling.py
+++ service/ExceptionHandling.py
@@ -1,17 +1,10 @@
 import traceback
-import OwnErrors
 from sys import exit
 
 
 class SelectException(Exception):
 
     __str__ = "Select Returned No Valid Socket"
-
-
-def print_error_code(err: int):
-    """For error, where the exception string is less useful
-    and the traceback is not needed"""
-    print(OwnErrors.error_code(err))
 
 
 def handle_exception_and_exit(e: Exception, exitcode):
