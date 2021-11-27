4abefa46433a84ee1f0627f272a577504bc9187b
Levi Sasse <dvnbxd_78@mailbox.tu-berlin.de>
2021-11-27 15:31:25 +0100

    more classes

--- service/sending.py
+++ service/sending.py
@@ -3,45 +3,49 @@
 from exception_handling import print_exception_str
 from socket_wrapper import SocketWrap
 
-buffer_size: int = 512
-message_length: int = 1024
 
-close_bytes: bytes = b"\r\n"
+class Transmitter:
 
+    client: SocketWrap
 
-def send_bytes_to_socket(c_socket: SocketWrap, msg: bytes) -> int:
-    """Utilizing send() in a while loop: send(msg[bytes_send:]).
-    Also catching BrokenPipeError.
-    """
-    bytes_send = 0
-    while bytes_send < len(msg):
-        try:
-            bytes_send += c_socket.send(msg[bytes_send:])
-        except BrokenPipeError as bpe:
-            print_exception_str(bpe)
-        if not bytes_send:
-            break
-    return bytes_send
+    buffer_size: int = 512
+    message_length: int = 1024
+    close_bytes: bytes = b"\r\n"
 
+    def set_client(self, client):
+        self.client = client
 
-def receive_bytes_from_socket(c_socket: SocketWrap) -> bytes:
-    """Calling recv() in a while loop using a bytearray to append
-    the received buffer to a result value witch can be returned.
-    """
-    bytes_received = 0
-    request: bytearray = bytearray(b"")
-    while bytes_received < message_length:
-        client_data: bytes = c_socket.receive(buffer_size)
-        if not client_data or client_data == b"":
-            return b""  # or break?
-        bytes_received += len(client_data)
-        request.extend(client_data)
-        if received_closing_bytes(client_data):
-            request = request[:-len(close_bytes)]
-            break
-    return bytes(request)
+    def send_bytes_to_socket(self, msg: bytes) -> int:
+        """Utilizing send() in a while loop: send(msg[bytes_send:]).
+        Also catching BrokenPipeError.
+        """
+        bytes_send = 0
+        while bytes_send < len(msg):
+            try:
+                bytes_send += self.client.send(msg[bytes_send:])
+            except BrokenPipeError as bpe:
+                print_exception_str(bpe)
+            if not bytes_send:
+                break
+        return bytes_send
 
+    def receive_bytes_from_socket(self) -> bytes:
+        """Calling recv() in a while loop using a bytearray to append
+        the received buffer to a result value witch can be returned.
+        """
+        bytes_received = 0
+        request: bytearray = bytearray(b"")
+        while bytes_received < self.message_length:
+            client_data: bytes = self.client.receive(self.buffer_size)
+            if not client_data or client_data == b"":
+                return b""  # or break?
+            bytes_received += len(client_data)
+            request.extend(client_data)
+            if self.received_closing_bytes(client_data):
+                request = request[:-len(self.close_bytes)]
+                break
+        return bytes(request)
 
-def received_closing_bytes(received_bytes: bytes) -> bool:
-    if received_bytes[-len(close_bytes):] == close_bytes:
-        return True
+    def received_closing_bytes(self, received_bytes: bytes) -> bool:
+        if received_bytes[-len(self.close_bytes):] == self.close_bytes:
+            return True
--- service/service.py
+++ service/service.py
@@ -1,47 +1,42 @@
 """The module which implements the communication protocol/functionality"""
 
-import exception_handling as exc
-from socket_wrapper import SocketWrap
-
-from sending import receive_bytes_from_socket
-from sending import send_bytes_to_socket
-from sending import close_bytes
+from exception_handling import handle_exception_and_exit
+import sending
 import cryptic
 
 
 class Service:
 
-    client: SocketWrap
-
     def __init__(self, client):
-        self.client = client
+        self.transmit = sending.Transmitter()
+        self.transmit.set_client(client)
 
     def digest_client_request_and_send_back(self):
         """receive some data, digest it and send it back"""
-        client_request: bytes = receive_bytes_from_socket(self.client)
+        client_request: bytes = self.transmit.receive_bytes_from_socket()
         if client_request != b"":
             self._send_digested(client_request)
 
     def _send_digested(self, msg: bytes):
         """first sending the message then sending the closing bytes"""
-        send_bytes_to_socket(self.client, cryptic.digest(msg))
-        send_bytes_to_socket(self.client, close_bytes)
+        self.transmit.send_bytes_to_socket(cryptic.digest(msg))
+        self.transmit.send_bytes_to_socket(self.transmit.close_bytes)
 
     def close_connection_and_del_client_elem(self, client_list):
         """Closes socket connection and removes the socket from the ready clients list"""
         try:
             self._del_client_elem(client_list)
-            if self.client is not None:
-                self.client.close()
+            if self.transmit.client is not None:
+                self.transmit.client.close()
         except Exception as e:
-            exc.handle_exception_and_exit(e, 700)
+            handle_exception_and_exit(e, 700)
 
     def _del_client_elem(self, client_list):
         """Calls the pop() method to delete the element in the lists of the clients"""
         try:
-            list_index = client_list.index(self.client.socket_obj)
+            list_index = client_list.index(self.transmit.client.socket_obj)
             client_list.pop(list_index)
         except IndexError as ie:
-            exc.handle_exception_and_exit(ie, 6000)
+            handle_exception_and_exit(ie, 6000)
         except ValueError as ve:
-            exc.handle_exception_and_exit(ve, 6001)
+            handle_exception_and_exit(ve, 6001)
