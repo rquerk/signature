0571fa28894a90b55e40cdc1dbe542a61b4d3f4a
Levi Sasse <dvnbxd_78@mailbox.tu-berlin.de>
2021-12-13 22:12:32 +0100

    client automation

--- tests/client.py
+++ tests/client.py
@@ -7,18 +7,42 @@ import client_lib as service
 host = "localhost"
 port = 5421
 
-soc = service.socket_create()
-remote_ip = socket.gethostbyname(host)
-soc.connect((remote_ip, port))
 
-request = str(sys.argv[1])
-service.send_bytes_to_socket(soc, request.encode(service.encoding_type))
-service.send_bytes_to_socket(soc, service.close_bytes)
+def new_socket():
+    soc = service.socket_create()
+    remote_ip = socket.gethostbyname(host)
+    soc.connect((remote_ip, port))
+    return soc
 
-response = service.receive_bytes_from_socket(soc)
+def read_input():
+    return str(sys.argv[1])
+    
+def send_input_to_server(soc):
+    request = read_input()
+    service.send_bytes_to_socket(soc, request.encode(service.encoding_type))
+    service.send_bytes_to_socket(soc, service.close_bytes)
 
-soc.shutdown(socket.SHUT_RDWR)
-soc.close()
+def receive_response(soc):
+    return service.receive_bytes_from_socket(soc)
 
-print(fr"{response}", file=sys.stdout)
-# .decode("utf_8", errors="ignore")
+def close_socket(soc):
+    soc.shutdown(socket.SHUT_RDWR)
+    soc.close()
+    
+if __name__ == "__main__":
+    
+    soc = new_socket()
+    send_input_to_server(soc)
+    response = receive_response(soc)
+    close_socket(soc)
+    
+    soc2 = new_socket()
+    send_input_to_server(soc2)
+    response2 = receive_response(soc2)
+    close_socket(soc2)
+    
+    if response != response2:
+        exit(-1)
+        
+    print(fr"{response, response2}", file=sys.stdout)
+    # .decode("utf_8", errors="ignore")
